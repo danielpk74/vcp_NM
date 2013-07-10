c87f72ae9bbddd54ca756adcc0d328a0d819d924
dmontol <dmontol@DMONTOL.epmtelco.com.co>
2013-07-10 08:15:03 -0500

    [ACTUALIZACION]: Todos los meses para corregir problema en presentacion de los datos cuando la venta del producto no es realizaad desde enero.

--- protected/views/site/plantillas/detallesRegional.php
+++ protected/views/site/plantillas/detallesRegional.php
@@ -1,6 +1,19 @@
-<!--<h4><?php echo $opcion; ?> Por Regional</h4>-->
-<!--<hr>-->
-
+<?php 
+  //TODO Cambiar
+   $fechas = array(
+                array('MES' => 'Enero'),
+                array('MES' => 'Febrero'),
+                array('MES' => 'Marzo'),
+                array('MES' => 'Abril'),
+                array('MES' => 'Mayo'),
+                array('MES' => 'Junio'),
+                array('MES' => 'Julio'),
+                array('MES' => 'Agosto'),
+                array('MES' => 'Septiembre'),
+                array('MES' => 'Octubre'),
+                array('MES' => 'Noviembre'),
+                array('MES' => 'Diciembre'));
+?>
 <!-- COMIENZA INGRESOS POR REGIONAL -->
 <div class="tabbable"> 
     <ul class="nav nav-tabs">
@@ -9,10 +22,10 @@
     </ul>
     <div class="tab-content">
         <div class="tab-pane active" id="ingresadas">
-            
-            <!--- INGRESADAS  X DIAS X REGIONAL-->
-            <h5>Evolución últimos 7 días X Regional</h5>
-            <?php
+            <?php if ($fechasIngresos != null) { ?>
+                <!--- INGRESADAS  X DIAS X REGIONAL-->
+                <h5>Evolución últimos 7 días X Regional</h5>
+                <?php
                 $categorias = FunsionesSoporte::GenerarCategoryXMLChart($fechasIngresos, 'FECHA_INGRESO');
 
                 $dataSets = "";
@@ -24,67 +37,74 @@
 
                 $strXML3 = FunsionesSoporte::GenerarXML_ChartCombinedColumn('Ingresos X Regional', 'Cantidad Ingresada', $categorias, $dataSets);
                 echo "<center>" . renderChart(Yii::app()->request->baseUrl . "/utilidades/fusionchart/StackedColumn2DLine.swf", "", $strXML3, "IngresosRegional", '100%', 250, false, true) . "</center>";
-            ?>
-            
-            <hr>
-            
+                ?>
+
+                <hr>
+
+            <?php } ?>
+
             <!--Ingresadas X Mes X Regional-->
             <h5>Ingresadas X Mes X Regional</h5>
+
+            <?php
             
-             <?php
-            $categorias = FunsionesSoporte::GenerarCategoryXMLChart($fechasIngresosxMes, 'FECHA_INGRESO');
-            
+            $categorias = FunsionesSoporte::GenerarCategoryXMLChart($fechas, 'MES');
+
             $dataSets = "";
             for ($i = 0; $i < Count($ingresadasRegionalxMes); $i++) {
                 $regional = $ingresadasRegionalxMes[$i]['REGIONAL'];
                 unset($ingresadasRegionalxMes[$i]['REGIONAL']);
                 $dataSets .= FunsionesSoporte::GenerarValueXMLChart($ingresadasRegionalxMes[$i], $regional, 'CANTIDAD', false);
             }
-            
+
             $strXML3 = FunsionesSoporte::GenerarXML_ChartCombinedColumn('Ingresadas x Regional x Mes', 'Cantidad Ingresada', $categorias, $dataSets);
             echo "<center>" . renderChart(Yii::app()->request->baseUrl . "/utilidades/fusionchart/StackedColumn2DLine.swf", "", $strXML3, "IngresadasRegionalxMes", '100%', 250, false, true) . "</center>";
             ?>
-            
+
             <!--- FIN INGRESADAS -->
         </div>
-        
+
         <div class="tab-pane" id="instaladas">
-             <!--- INSTALADAS  X DIAS X REGIONAL-->
-            <h5>Evolución últimos 7 días X Regional</h5>
-            <?php
-            $categorias = FunsionesSoporte::GenerarCategoryXMLChart($fechasInstalaciones, 'FECHA_INSTALACION');
-                        
-            $dataSets = "";
-            for ($i = 0; $i < Count($instaladasRegional); $i++) {
-                $regional = $instaladasRegional[$i]['REGIONAL'];
-                unset($instaladasRegional[$i]['REGIONAL']);
-                $dataSets .= FunsionesSoporte::GenerarValueXMLChart($instaladasRegional[$i], $regional, 'CANTIDAD', false);
-            }
-            
-            $strXML3 = FunsionesSoporte::GenerarXML_ChartCombinedColumn('Instaladas X Regional', 'Cantidad Instalada', $categorias, $dataSets);
-           
-            echo "<center>" . renderChart(Yii::app()->request->baseUrl . "/utilidades/fusionchart/StackedColumn2DLine.swf", "", $strXML3, "InstalacionRegional", '100%', 250, false, true) . "</center>";
-            ?>
-             
-            <hr> 
-            
+
+            <?php if ($fechasIngresos != null) { ?>
+                <!--- INSTALADAS  X DIAS X REGIONAL-->
+                <h5>Evolución últimos 7 días X Regional</h5>
+                <?php
+                $categorias = FunsionesSoporte::GenerarCategoryXMLChart($fechasInstalaciones, 'FECHA_INSTALACION');
+
+                $dataSets = "";
+                for ($i = 0; $i < Count($instaladasRegional); $i++) {
+                    $regional = $instaladasRegional[$i]['REGIONAL'];
+                    unset($instaladasRegional[$i]['REGIONAL']);
+                    $dataSets .= FunsionesSoporte::GenerarValueXMLChart($instaladasRegional[$i], $regional, 'CANTIDAD', false);
+                }
+
+                $strXML3 = FunsionesSoporte::GenerarXML_ChartCombinedColumn('Instaladas X Regional', 'Cantidad Instalada', $categorias, $dataSets);
+
+                echo "<center>" . renderChart(Yii::app()->request->baseUrl . "/utilidades/fusionchart/StackedColumn2DLine.swf", "", $strXML3, "InstalacionRegional", '100%', 250, false, true) . "</center>";
+                ?>
+
+                <hr> 
+
+            <?php } ?>
+
             <!--Instaladas X Mes X Regional-->
             <h5>Instaladas X Mes X Regional</h5>
-            
+
             <?php
-            $categorias = FunsionesSoporte::GenerarCategoryXMLChart($fechasInstalacionesxMes, 'FECHA_INSTALACION');
-            
+            $categorias = FunsionesSoporte::GenerarCategoryXMLChart($fechas, 'MES');
+
             $dataSets = "";
             for ($i = 0; $i < Count($instaladasRegionalxMes); $i++) {
                 $regional = $instaladasRegionalxMes[$i]['REGIONAL'];
                 unset($instaladasRegionalxMes[$i]['REGIONAL']);
                 $dataSets .= FunsionesSoporte::GenerarValueXMLChart($instaladasRegionalxMes[$i], $regional, 'CANTIDAD', false);
             }
-            
+
             $strXML3 = FunsionesSoporte::GenerarXML_ChartCombinedColumn('Instaladas x Regional x Mes', 'Cantidad Instalada', $categorias, $dataSets);
             echo "<center>" . renderChart(Yii::app()->request->baseUrl . "/utilidades/fusionchart/StackedColumn2DLine.swf", "", $strXML3, "InstalacionesRegionalxMes", '100%', 250, false, true) . "</center>";
             ?>
-             
+
             <!--- FIN INSTALADAS-->
         </div>
     </div>
